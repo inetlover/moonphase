13093fbfd517e4555ba4b22f7ddb4e50f30a60d5
Dominique Feyer <dfeyer@ttree.ch>
2012-01-25 16:08:18 +0100

    [TASK] Create Classes subdirectory

--- moonphase.php
+++ moonphase.php
@@ -1,18 +1,20 @@
 <?php
 
-require('moonphase.inc.php');
+require('Classes/Moonphase.php');
 
 $moonphase = new \TTREE\moonphase();
 
 // phaseHunting() Example
 print "<pre>";
 print "Example: phaseHunting()\n";
+print "-----------------------\n";
 $moonphase->calculatePhaseHunting();
 print "\n\n";
 
 
 // findPhaseList() Example
 print "Example: findPhaseList()\n";
+print "------------------------\n";
 $startDate = new DateTime("2012-01-01 00:00:00 CEST");
 $endDate = new DateTime("2012-12-01 00:00:00 CEST");
 $moonphase->calculatePhaseList($startDate, $endDate);
@@ -23,6 +25,7 @@ print "\n\n";
 $date = new DateTime('2012-01-01 00:00:00 CEST');
 
 print "Example: calculatePhase()\n";
+print "-------------------------\n";
 $moonphase->calculatePhaseByDate($date);
 print "</pre>\n\n";
 
